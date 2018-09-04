3b02f6e2b46b5fa07055f4e9684fa3e83965feda
chaky <chaky@asdcode.com>
2018-09-04 18:26:16 +0300

    added test.sh to the repo

---
+++ bin/test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#sleep 1
+#cd ../
+#ls
+./log2sqlite sqlite_db="./sqlite_db.sqlite"  log_file="/var/log/nginx/../nginx/vint_admins_requests.log"
+
+#return 0
+
+# dir >> /home/pacman/www/pacman_front/cgi-bin/admin/front/log/need_deploy.log
+# chmod 666 logs/need_deploy.log
+
+#find ./img -type f -exec chmod 666 {} \;
